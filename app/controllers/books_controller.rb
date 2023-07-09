8c9ab125ba0cf4e3296c3ef815538bd826fb5adc
AkiraKoikeda <AkiraKoikedaAkiraKoikeda135214468+AkiraKoikeda@users.noreply.github.com>
2023-07-09 07:42:33 +0000

    [Fix]bookers非同期通信実装途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,8 +10,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      respond_to do |format|
+        format.html
+          format.js
+      end
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
     else
       @books =Book.all
       render :index
